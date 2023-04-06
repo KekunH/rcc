2eba3dab6962472f57db14303534ed9f229c64d7
Kekun Han <kakua@Kekuns-MacBook-Air.local>
2023-04-06 11:56:21 -0500

    H1

--- Q2b_mpi.py
+++ Q2b_mpi.py
@@ -17,40 +17,40 @@ T = int(4160)
 
 @jit(nopython = True)
 def sim_fast_rhos(rhos, mu, sigma, S, T, eps_mat):
-  z_mat = np.zeros((T,S))
-  z_0 = mu - 3*sigma
-  rho_time = []
-  for rho in rhos:
-    lst = []
-    for s_ind in range(S):
-      z_tm1 = z_0
-      for t_ind in range(T):
-        e_t = eps_mat[t_ind, s_ind]
-        z_t = rho * z_tm1 + (1 - rho) * mu + e_t
-        z_tm1 = z_t
-        if z_tm1 <= 0:
-          lst.append(t_ind)
-          break
-    rho_time.append((np.array(lst).mean(), rho))
-             
-  return rho_time
+    z_0 = mu - 3*sigma
+    rho_time = []
+    for rho in rhos:
+        lst = []
+        for s_ind in range(S):
+            z_tm1 = z_0
+            for t_ind in range(T):
+                e_t = eps_mat[t_ind, s_ind]
+                z_t = rho * z_tm1 + (1 - rho) * mu + e_t
+                z_tm1 = z_t
+                if z_tm1 <= 0:
+                    lst.append(t_ind)
+                    break
+                else:
+                    if t_ind == T - 1:
+                        lst.append(t_ind)
+        rho_time.append((np.array(lst).mean(), rho))
+
+    return rho_time
 
 data = None
 eps_mat = None
 if rank == 0:
-  np.random.seed(rank)
-  eps_mat = sts.norm.rvs(loc = 0, scale = sigma, size = (T,S))
-  data = np.linspace(-0.95, 0.95, 200)
+    np.random.seed(rank)
+    eps_mat = sts.norm.rvs(loc = 0, scale = sigma, size = (T,S))
+    data = np.linspace(-0.95, 0.95, 200)
 subdata = np.empty(20)
-
 comm.Scatter(data, subdata, root = 0)
 eps_mat = comm.bcast(eps_mat, root = 0)
-
 result = np.array(sim_fast_rhos(subdata, mu, sigma, S, T, eps_mat))
 all_result = None
 if rank == 0:
-  all_result = np.empty((2,200))
+    all_result = np.empty((2,200))
 comm.Gather(result, all_result, root = 0)
 
 if rank == 0:
-  print(all_result)
+    print(all_result)
