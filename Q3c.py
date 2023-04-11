39781299936849dd3d3f6ca305621ffef475cebe
Kekun Han <kakua@Kekuns-Air.attlocal.net>
2023-04-10 21:28:53 -0500

    new

--- Q3c.py
+++ Q3c.py
@@ -11,10 +11,14 @@ band5 = rasterio.open("/project2/macs30123/landsat8/LC08_B5.tif") #nir
 # Convert nir and red objects to float64 arrays
 red = band4.read(1).astype("float64")
 nir = band5.read(1).astype("float64")
+red = np.tile(red, 20)
+nir = np.tile(nir, 20)
+
 start = time.time()
 ndvi = (nir - red) / (nir + red)
 end = time.time()
 print(end - start)
+
 # NDVI calculation
 start = time.time()
 queue = cl.CommandQueue(ctx)
