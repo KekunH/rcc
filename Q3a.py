2698aa2b7628dab27fb66508b5b153adc6539492
Kekun Han <kakua@Kekuns-Air.attlocal.net>
2023-04-10 20:53:25 -0500

    new

---
+++ Q3a.py
@@ -0,0 +1,28 @@
+import rasterio
+import numpy as np
+import pyopencl as cl
+import pyopencl.array as cl_array
+import time
+from pyopencl.elementwise import ElementwiseKernel
+
+# Import bands as separate images; in /project2/macs30123 on Midway2
+band4 = rasterio.open("/project2/macs30123/landsat8/LC08_B4.tif") #red
+band5 = rasterio.open("/project2/macs30123/landsat8/LC08_B5.tif") #nir
+
+start = time.time()
+# Convert nir and red objects to float64 arrays
+red = band4.read(1).astype("float64")
+nir = band5.read(1).astype("float64")
+# NDVI calculation
+ctx = cl.create_some_context()
+queue = cl.CommandQueue(ctx)
+lin_comb = ElementwiseKernel(ctx,
+                             "float *x, float *y, float *ndvi",
+                             "ndvi[i] = (x[i] - y[i])/(x[i] + y[i])")
+red_cl = cl_array.to_device(queue, red)
+nir_cl = cl_array.to_device(queue, nir)
+ndvi = cl.array.empty_like(red_cl)
+lin_comb(nir_cl, red_cl, ndvi)
+result_np = ndvi.get()
+end = time.time()
+print(end - start)
